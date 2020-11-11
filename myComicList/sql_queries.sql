c075bc85efcfcb48a70bbbed526eebd65152fbda
tbrittain2 <tbrittain2@ucmerced.edu>
2020-11-10 18:16:33 -0800

    create/drop completed

---
+++ myComicList/sql_queries.sql
@@ -0,0 +1,54 @@
+--Create all tables
+CREATE TABLE Issues (
+    i_id decimal(9,0) NOT NULL PRIMARY KEY,
+    i_title char(50) NOT NULL,
+    i_artistId decimal(9,0) NOT NULL,
+    i_variant char(50), --considering removing this
+    i_description char(200) NOT NULL,
+    i_date date(4,0) NOT NULL,
+    i_srp decimal(2,2) NOT NULL
+);
+
+CREATE TABLE ReadingList(
+    rl_readerID decimal(9,0) NOT NULL,
+    rl_issueID char(4)  NOT NULL,
+    rl_ownStat char(10) NOT NULL
+);
+
+CREATE TABLE FollowList(
+    fl_id decimal(9,0) NOT NULL PRIMARY KEY,
+    --Do i need to change this part?
+    fl_artistID decimal(9,0) NOT NULL,
+    fl_writerID decimal(9,0) NOT NULL
+);
+
+CREATE TABLE Artist(
+    a_id decimal(9,0) NOT NULL PRIMARY KEY,
+    a_name char(50) NOT NULL
+);
+
+CREATE TABLE Writer(
+    w_id decimal(9,0) NOT NULL PRIMARY KEY,
+    w_name char(50) NOT NULL
+);
+
+CREATE TABLE ReccList(
+    r_aId decimal(9,0) NOT NULL,
+    r_wId decimal(9,0) NOT NULL,
+    r_readerID decimal(9,0) NOT NULL,
+    r_issueID decimal(9,0) NOT NULL
+)
+
+--Drop all tables
+DROP TABLE Issues;
+DROP TABLE ReadingList;
+DROP TABLE FollowList;
+DROP TABLE Artist;
+DROP TABLE Writer;
+DROP TABLE ReccList;
+
+
+
+
+
+
