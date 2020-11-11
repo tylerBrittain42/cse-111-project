d7ff6b581d6a7ce9fba851f2127d92e5dd0c5a94
tbrittain2 <tbrittain2@ucmerced.edu>
2020-11-10 21:58:11 -0800

    updated db and added missing table

--- myComicList/sql_queries.sql
+++ myComicList/sql_queries.sql
@@ -7,6 +7,10 @@ CREATE TABLE Issues (
     i_srp decimal(2,2) NOT NULL
 );
 
+CREATE TABLE readerList(
+    r_id  decimal(9,0) NOT NULL PRIMARY KEY,
+    r_name char(50) NOT NULL);
+
 CREATE TABLE ReadingList(
     rl_readerID decimal(9,0) NOT NULL,
     rl_issueID char(4)  NOT NULL,
@@ -48,5 +52,14 @@ DROP TABLE ReccList;
 
 
 
+
+
+
+
+
+
+.mode "csv"
+.separator "\t"
+.import /home/tyler/Documents/cse-111/final_project/cse-111-project/data/pull_w_tabs.txt Issues
 
 
